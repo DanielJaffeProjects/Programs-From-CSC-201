dc88dcbcb2b2e590be3b352c3854b59228199e99
DanielJaffeCSCMajor <daniel.jaffe@spartans.ut.edu>
2024-08-01 09:10:42 -0400

    Started program 5 a sci fi library and open and read the book and added files necessary

---
+++ Program 5 SciFiLib.py
@@ -0,0 +1,31 @@
+#########################
+# Program 5 SciFiLib
+# Daniel Jaffe
+# Date created: 7/31/24
+#########################
+
+
+
+Debug = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+#######
+# Main
+#######
+
+with open("books.txt","r") as books:
+    books.readlines()
+
+if Debug == True:
+    print(books)
