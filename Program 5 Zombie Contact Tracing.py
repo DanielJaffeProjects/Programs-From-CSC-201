f983cb7739a7d76eec46cf2174a8325397a1ebe7
DanielJaffeCSCMajor <daniel.jaffe@spartans.ut.edu>
2024-08-07 10:05:15 -0400

    Part 1 complete for who the sick person has been in contact with

--- Program 5 Zombie Contact Tracing.py
+++ Program 5 Zombie Contact Tracing.py
@@ -4,38 +4,62 @@
 # Date created: 7/31/24
 #########################
 
+# Imports
 from CSC201UT import *
 
 Debug = True
 
 # Part 1
-# Used an unorder list since we had a small amount of data and amount of data was predictable
-def contact_with(set1):
+# I used a array since tranversing to through a array has a O(n)
+def contact_with(first_name_array,contact_array):
 
-    for i in range (len(set1)):
-        print(i)
-
-    # output of each contact person 1 is in contact with
-    print(f"{set1[0]} had contact with {set1[1:]}")
+    print("Contact records:")
 
 
 
+    # loop through each line
+    # Connect first name with contacts
+    for i in range(0,len(first_name_array)):
+        print(f"  {first_name_array[i]} had contact with {", ".join(contact_array[i])}")
 
+# I choose a link list since it is a small data set but it is unpredictable
+def patient_zeros(set1):
+    pass
 
-
-
-
-
-#######
 # Main
 #######
 
 with open("DataSet1.txt","r") as file:
-    set1 = file.read().split('\n')
+    set1 = file.read().splitlines()
 
 if Debug == True:
     print(set1)
 
+# initialize arrays
+people_array = []
+first_name_array = []
+contact_array = []
 
-# outputs who each person had contact with
-contact_with(set1)
+# loop through each line in the set
+for line in (set1):
+
+    # Split into arrays for first name and contacts
+    people = line.split(",")
+    people_array.append(people)
+    first_name_array.append(people[0])
+    contact_array.append(people[1:])
+    contact_array.sort()
+
+    if Debug == True:
+        print("people", people)
+
+if Debug == True:
+    print("people_array", people_array)
+    print("first_name_array", first_name_array)
+    print("contact_array", contact_array)
+
+# Part 1 Who did each sick person have contact with
+contact_with(first_name_array,contact_array)
+
+# Part 2 Who are patient zeros
+patient_zeros()
